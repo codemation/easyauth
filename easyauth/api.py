63ef4b65f82f143df013a6436c156d92f5bf73e3
Joshua Jamison <joshua.jamison@ecorus.com>
2021-03-06 00:21:59 +0100

    fixed issue with updating an existing user's groups list, added verification for updates of list items in Users, Groups, Roles, added create_service endpoint for handling service account creation, create user now only creates account_type 'user' users

--- easyauth/api.py
+++ easyauth/api.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from easyauth.models import User, Group, Role, Permission
+from easyauth.models import User, Service, Group, Role, Permission
 
 
 async def api_setup(server):
@@ -16,6 +16,24 @@ async def api_setup(server):
     groups_tb = server.db.tables['groups']
     roles_tb = server.db.tables['roles']
     permissions_tb = server.db.tables['permissions']
+
+    async def verify_user(user):
+        if await users_tb[user] is None:
+            # raise group does not exist
+            raise HTTPException(status_code=400, detail=f"no user with name {user} exists")
+    async def verify_group(group):
+        if await groups_tb[group] is None:
+            # raise group does not exist
+            raise HTTPException(status_code=400, detail=f"no group with name {group} exists, create first")
+    async def verify_role(role):
+        if await roles_tb[role] is None:
+            # raise group does not exist
+            raise HTTPException(status_code=400, detail=f"no role with name {role} exists, create first")
+    async def verify_action(action):
+        if await permissions_tb[action] is None:
+            # raise group does not exist
+            raise HTTPException(status_code=400, detail=f"no action with name {action} exists, create first")
+
 
     @server.get('/auth/serviceaccount/token/{service}', response_model=Token, tags=['Token'])
     async def get_service_account_token(service: str):
@@ -61,53 +79,78 @@ async def api_setup(server):
         if not await users_tb[user['username']] is None:
             raise HTTPException(status_code=400, detail=f"{user['username']} already exists")
         for group in user['groups']['groups']:
-            if await groups_tb[group] is None:
-                # raise group does not exist
-                raise HTTPException(status_code=400, detail=f"no group with name {group} exists, create first")
+            await verify_group(group)
 
         # encode password before storing
         user['password'] = server.encode_password(user['password'])
+        user['account_type'] = 'user'
 
         await users_tb.insert(**user)
 
         # trigger some activation email later?
 
         return f"{user['username']} created"
+    @server.put('/auth/service', status_code=201, actions=['CREATE_USER'], tags=['Users'])
+    async def create_service(service: Service):
+        service = dict(service)
+        if not await users_tb[service['username']] is None:
+            raise HTTPException(status_code=400, detail=f"{service['username']} already exists")
 
+        for group in service['groups']['groups']:
+            await verify_group(group)
+        
+        service['account_type'] = 'service'
+
+        await users_tb.insert(**service)
+        # trigger some activation email later?
+
+        return f"{service['username']} created"
+
+        
+
+    class UserGroup(BaseModel):
+        groups: list
 
     class UserUpdate(BaseModel):
         full_name: Optional[str]
         password: Optional[str]
         email: Optional[str]
-        groups: Optional[list]
+        groups: Optional[UserGroup]
 
     @server.post('/auth/user/{username}', tags=['Users'])
     async def update_user(
         username: str,
         update: UserUpdate
     ):
-
+        await verify_user(username)
+        
         update = {k: v for k, v in dict(update).items() if not v is None}
+
+        to_update = {}
+        for k, v in dict(update).items():
+            if k == 'groups':
+                to_update[k] = dict(v)
+                for group in to_update[k]['groups']:
+                    await verify_group(group)
+            else:
+                to_update[k] = v
+                
+        server.log.warning(f"update: {to_update}")
+        update = to_update
 
         if "password" in update:
             # encode password before storing
             update['password'] = server.encode_password(update['password'])
 
-        if not await users_tb[username] is None:
-            await users_tb.update(
-                where={'username': username},
-                **update
-            )
-            return f"{username} updated"
-        raise HTTPException(
-            status_code=404,
-            detail=f"no user with name {username} exists"
+        await users_tb.update(
+            where={'username': username},
+            **update
         )
+        return f"{username} updated"
 
     @server.delete('/auth/user', tags=['Users'])
     async def delete_user(username: str):
-        if await users_tb[username] is None:
-            raise HTTPException(status_code=404, detail=f"no user found with name {username}")
+        await verify_user(user)
         await users_tb.delete(where={'username': username})
         return f"{username} deleted"
 
@@ -117,9 +160,8 @@ async def api_setup(server):
 
     @server.get('/auth/users/{username}', tags=['Users'])
     async def get_user(username: str):
+        await verify_user(user)
         user = await users_tb[username]
-        if user is None:
-            raise HTTPException(status_code=404, detail=f"no user found with name {username}")
         return user
 
     # Groups
@@ -132,31 +174,29 @@ async def api_setup(server):
             raise HTTPException(status_code=400, detail=f"{group['group_name']} already exists")
 
         for role in group['roles']['roles']:
-            if await roles_tb[role] is None:
-                # raise group does not exist
-                raise HTTPException(status_code=400, detail=f"no role with name {role} exists, create first")
+            await verify_role(role)
 
         await groups_tb.insert(**group)
         return f"group {group['group_name']} created"
 
+    class UpdateRoles(BaseModel):
+        roles: list
 
     @server.post('/auth/groups', status_code=201, tags=['Groups'])
-    async def create_or_update_group(group: Group):
-        group = dict(group)
-        if not await groups_tb[group['group_name']] is None:
-            await groups_tb.update(
-                where={'group_name': group.pop('group_name')},
-                **group
-            )
-            return f"existing group updated"
-
-        await groups_tb.insert(**group)
-        return f"group {group['group_name']} created"
+    async def update_group(group: str, roles: UpdateRoles):
+        roles = dict(roles)
+        await verify_group(group)
+        for role in roles['roles']:
+            await verify_role(role)
+        await groups_tb.update(
+            roles={'roles': roles},
+            where={'group_name': group}
+        )
+        return f"existing group updated"
         
     @server.delete('/auth/group', tags=['Groups'])
-    async def delete_group(group_name):
-        if await groups_tb[group_name] is None:
-            raise HTTPException(status_code=404, detail=f"no group found with name {group_name}")
+    async def delete_group(group_name: str):
+        await verify_group(group_name)
         await groups_tb.delete(where={'group_name': group_name})
         return f"{group_name} deleted"
 
@@ -166,11 +206,9 @@ async def api_setup(server):
 
     @server.get('/auth/groups/{group}', tags=['Groups'])
     async def get_group(group_name: str):
+        await verify_group(group_name)
         group = await groups_tb[group_name]
-        if group is None:
-            raise HTTPException(status_code=404, detail=f"no group found with name {group_name}")
         return group
-
 
     # Roles
 
@@ -182,30 +220,30 @@ async def api_setup(server):
             raise HTTPException(status_code=400, detail=f"{role['role']} already exists")
         
         for permission in role['permissions']['actions']:
-            if await permissions_tb[permission] is None:
-                # raise group does not exist
-                raise HTTPException(status_code=400, detail=f"no permission with name {permission} exists, create first")
+            await verify_action(permission)
         
         await roles_tb.insert(**role)
         return f"role {role['role']} created"
 
-    @server.post('/auth/roles', status_code=201, tags=['Roles'])
-    async def create_or_update_role(role: Role):
-        role = dict(role)
-        if not await roles_tb[role['role']] is None:
-            await roles_tb.update(
-                where={'role': role.pop('role')},
-                **role
-            )
-            return f"existing role updated"
+    class UpdateActions(BaseModel):
+        actions: list
 
-        await roles_tb.insert(**role)
-        return f"role {role['role']} created"
+    @server.post('/auth/roles', status_code=201, tags=['Roles'])
+    async def update_role(role: str, actions: UpdateActions):
+        actions = dict(actions)
+        await verify_role(role)
+        for action in actions['actions']:
+            await verify_action(action)
+
+        await roles_tb.update(
+            permissions={'actions': actions},
+            where={'role': role.pop('role')}
+        )
+        return f"existing role updated"
 
     @server.delete('/auth/role', tags=['Roles'])
-    async def delete_role(role):
-        if await roles_tb[action] is None:
-            raise HTTPException(status_code=404, detail=f"no role found with name {role}")
+    async def delete_role(role: str):
+        await verify_role(role)
         await roles_tb.delete(where={'role': role})
         return f"{role} deleted"
 
@@ -215,12 +253,10 @@ async def api_setup(server):
 
     @server.get('/auth/roles/{role}', tags=['Roles'])
     async def get_role(role: str):
+        await verify_role(role)
         role = await roles_tb[role]
-        if role is None:
-            raise HTTPException(status_code=404, detail=f"no role found with name {role}")
         return role
 
-    
     ## Permissions 
 
     @server.put('/auth/permissions', status_code=201, tags=['Actions'])
@@ -232,22 +268,22 @@ async def api_setup(server):
         await permissions_tb.insert(**permission)
         return f"permission {permission['action']} created"
 
-    @server.post('/auth/permissions', status_code=201, tags=['Actions'])
-    async def create_or_update_permission(permission: Permission):
-        permission = dict(permission)
-        if not await permissions_tb[permission['action']] is None:
-            await permissions_tb.update(
-                where={'action': permission.pop('action')},
-                **permission
-            )
-            return f"existing permission updated"
+    class UpdateDetails(BaseModel):
+        detail: str
 
-        await permissions_tb.insert(**permission)
-        return f"permission {permission['action']} created"
+    @server.post('/auth/permissions', status_code=201, tags=['Actions'])
+    async def update_permission(action: str, detail: UpdateDetails):
+        detail = dict(detail)
+        await verify_action(action)
+        await permissions_tb.update(
+            details=detail['detail'],
+            where={'action': action},
+        )
+        return f"existing permission updated"
+
     @server.delete('/auth/permission', tags=['Actions'])
     async def delete_permission(action):
-        if await permissions_tb[action] is None:
-            raise HTTPException(status_code=404, detail=f"not action found with name {action}")
+        await verify_action(action)
         await permissions_tb.delete(where={'action': action})
         return f"{action} deleted"
 
@@ -257,7 +293,6 @@ async def api_setup(server):
 
     @server.get('/auth/permission/{action}', tags=['Actions'])
     async def get_permission(action: str):
+        await verify_action(action)
         permission = await permissions_tb[action]
-        if permission is None:
-            raise HTTPException(status_code=404, detail=f"not action found with name {action}")
         return permission
