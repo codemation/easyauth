48dbd818928e866125578a2e01c23d1984201b19
Dylan Leroux <contact.dylanleroux@gmail.com>
2023-01-30 18:52:11 +0100

    Default value for when default_permissions is not provided

--- easyauth/server.py
+++ easyauth/server.py
@@ -961,7 +961,7 @@ class EasyAuthServer:
 
         return auth_endpoint
 
-    def parse_permissions(self, users, groups, roles, actions, default_permissions):
+    def parse_permissions(self, users, groups, roles, actions, default_permissions=None):
         """
         returns permssions defined on a given endpoint if set
         if unset
