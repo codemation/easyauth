9e568f04e7ffe0cc7d314c673ba2eb36dccaab61
Joshua (codemation) <joshua.jamison@nostos-genomics.com>
2023-02-13 14:03:25 +0100

    reverted client to non-async

--- tests/test_server_api.py
+++ tests/test_server_api.py
@@ -1,34 +1,31 @@
-import asyncio
-
 import pytest
 
 from easyauth.models import Actions, Groups, Roles, Users
 
-from .conftest import AsyncClient
+from .conftest import TestClient
 
 
 @pytest.mark.asyncio
-async def test_server_authentication(auth_test_server: AsyncClient):
+async def test_server_authentication(auth_test_server: TestClient):
     test_client = auth_test_server
     server = test_client.app
 
     # verify endpoint access fails without token
-
-    response = await test_client.get("/finance/")
+    response = test_client.get("/finance/")
     assert response.status_code == 401, f"{response.text} - {response.status_code}"
 
     # verify token generation with bad credentials
 
     bad_credentials = {"username": "admin", "password": "BAD"}
 
-    response = await test_client.post("/auth/token/login", json=bad_credentials)
+    response = test_client.post("/auth/token/login", json=bad_credentials)
     assert response.status_code == 401, f"{response.text} - {response.status_code}"
 
     # verify token generation with correct credentials
 
     good_credentials = {"username": "admin", "password": "easyauth"}
 
-    response = await test_client.post("/auth/token/login", json=good_credentials)
+    response = test_client.post("/auth/token/login", json=good_credentials)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     token = response.json()
@@ -39,9 +36,8 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     headers = {"Authorization": f"Bearer {token['access_token']}"}
     # allow token to replicate
-    await asyncio.sleep(5)
 
-    response = await test_client.get("/finance/", headers=headers)
+    response = test_client.get("/finance/", headers=headers)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # test user creation
@@ -54,23 +50,23 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     }
 
     # check if user exists, delete if existing
-    response = await test_client.get("/auth/users/john", headers=headers)
+    response = test_client.get("/auth/users/john", headers=headers)
     assert response.status_code in {
         200,
         404,
     }, f"{response.text} - {response.status_code}"
     if response.status_code == 200:
         # delete user
-        response = await test_client.delete("/auth/user?username=john", headers=headers)
+        response = test_client.delete("/auth/user?username=john", headers=headers)
         assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # create user
 
-    response = await test_client.put("/auth/user", headers=headers, json=new_user)
+    response = test_client.put("/auth/user", headers=headers, json=new_user)
     assert response.status_code == 201, f"{response.text} - {response.status_code}"
 
     # test updating user
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/user/john",
         headers=headers,
         json={"full_name": "john j doe", "password": "new1234"},
@@ -79,15 +75,15 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # test deleting user
-    response = await test_client.delete("/auth/user?username=john", headers=headers)
+    response = test_client.delete("/auth/user?username=john", headers=headers)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # re-create & test token login with new user
 
-    response = await test_client.put("/auth/user", headers=headers, json=new_user)
+    response = test_client.put("/auth/user", headers=headers, json=new_user)
     assert response.status_code == 201, f"{response.text} - {response.status_code}"
 
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/token/login",
         json={"username": new_user["username"], "password": new_user["password"]},
     )
@@ -96,30 +92,30 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     token = response.json()
     headers = {"Authorization": f"Bearer {token['access_token']}"}
 
-    response = await test_client.get("/finance/", headers=headers)
+    response = test_client.get("/finance/", headers=headers)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # test permission creation
 
     # check if action exists, delete if existing
-    response = await test_client.get("/auth/actions/BASIC_CREATE", headers=headers)
+    response = test_client.get("/auth/actions/BASIC_CREATE", headers=headers)
     assert response.status_code in {
         200,
         404,
     }, f"{response.text} - {response.status_code}"
     if response.status_code == 200:
-        # delete user
-        response = await test_client.delete(
+        # delete action
+        response = test_client.delete(
             "/auth/action?action=BASIC_CREATE", headers=headers
         )
         assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     new_action = {"action": "BASIC_CREATE", "details": "BASIC CREATE action"}
-    response = await test_client.put("/auth/actions", headers=headers, json=new_action)
+    response = test_client.put("/auth/actions", headers=headers, json=new_action)
     assert response.status_code == 201, f"{response.text} - {response.status_code}"
 
     # test updating permission
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/actions?action=BASIC_CREATE",
         headers=headers,
         json={"details": "BASIC CREATE updated"},
@@ -130,14 +126,14 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     # test role creation
 
     # check if role exists, delete if existing
-    response = await test_client.get("/auth/roles/basic", headers=headers)
+    response = test_client.get("/auth/roles/basic", headers=headers)
     assert response.status_code in {
         200,
         404,
     }, f"{response.text} - {response.status_code}"
     if response.status_code == 200:
         # delete role
-        response = await test_client.delete("/auth/role?role=basic", headers=headers)
+        response = test_client.delete("/auth/role?role=basic", headers=headers)
         assert (
             response.status_code == 200
         ), f"delete role {response.text} - {response.status_code}"
@@ -146,13 +142,13 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     new_role = {"role": "basic", "actions": ["BASIC_CREATE"]}
 
-    response = await test_client.put("/auth/role", headers=headers, json=new_role)
+    response = test_client.put("/auth/role", headers=headers, json=new_role)
     assert (
         response.status_code == 201
     ), f"create role {response.text} - {response.status_code}"
 
     # test updating role
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/role/basic", headers=headers, json={"actions": ["BASIC_CREATE"]}
     )
 
@@ -163,14 +159,14 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     # create group
 
     # check if group exists, delete if existing
-    response = await test_client.get("/auth/groups/basic_users", headers=headers)
+    response = test_client.get("/auth/groups/basic_users", headers=headers)
     assert response.status_code in {
         200,
         404,
     }, f"get group{response.text} - {response.status_code}"
     if response.status_code == 200:
         # delete group
-        response = await test_client.delete(
+        response = test_client.delete(
             "/auth/group?group_name=basic_users", headers=headers
         )
         assert (
@@ -181,13 +177,13 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     new_group = {"group_name": "basic_users", "roles": ["basic"]}
 
-    response = await test_client.put("/auth/group", headers=headers, json=new_group)
+    response = test_client.put("/auth/group", headers=headers, json=new_group)
     assert (
         response.status_code == 201
     ), f"create group {response.text} - {response.status_code}"
 
     # test updating role
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/group/basic_users", headers=headers, json={"roles": ["basic", "admin"]}
     )
 
@@ -196,55 +192,56 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     ), f"update group {response.text} - {response.status_code}"
 
     # verify permission denied without required action
-    response = await test_client.get("/testing/actions", headers=headers)
+    response = test_client.get("/testing/actions", headers=headers)
+
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # verify permission denied without required role
-    response = await test_client.get("/testing/roles", headers=headers)
+    response = test_client.get("/testing/roles", headers=headers)
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # verify permission denied without required group
-    response = await test_client.get("/testing/groups", headers=headers)
+    response = test_client.get("/testing/groups", headers=headers)
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # test get_user {'Authorization': 'Bearer tokenstr'}
-    response = await test_client.get("/testing/current_user", headers=headers)
+    response = test_client.get("/testing/current_user", headers=headers)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
     assert "john" in response.text
 
     # test get_user - cookie only header
     token = headers["Authorization"].split(" ")[1]
     cookie_only = {"cookie": f"token={token}"}
-    response = await test_client.get("/testing/current_user", headers=cookie_only)
+    response = test_client.get("/testing/current_user", headers=cookie_only)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
     assert "john" in response.text
 
     # verify permission allowed for specific user
-    response = await test_client.get("/testing/", headers=headers)
+    response = test_client.get("/testing/", headers=headers)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     # add user to group with
 
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/user/john", headers=headers, json={"groups": ["basic_users"]}
     )
 
     # verify failures pre token generation
 
-    response = await test_client.get("/testing/actions", headers=headers)
+    response = test_client.get("/testing/actions", headers=headers)
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # verify permission denied without required action
-    response = await test_client.get("/testing/roles", headers=headers)
+    response = test_client.get("/testing/roles", headers=headers)
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # verify permission denied without required action
-    response = await test_client.get("/testing/groups", headers=headers)
+    response = test_client.get("/testing/groups", headers=headers)
     assert response.status_code == 403, f"{response.text} - {response.status_code}"
 
     # re create token with updated permissions
 
-    response = await test_client.post(
+    response = test_client.post(
         "/auth/token/login",
         json={"username": new_user["username"], "password": new_user["password"]},
     )
@@ -255,19 +252,19 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     # verify success with updated token
 
-    response = await test_client.get("/testing/actions", headers=headers)
+    response = test_client.get("/testing/actions", headers=headers)
     assert (
         response.status_code == 200
     ), f"new_token - actions {response.text} - {response.status_code}"
 
     # verify permission denied without required action
-    response = await test_client.get("/testing/roles", headers=headers)
+    response = test_client.get("/testing/roles", headers=headers)
     assert (
         response.status_code == 200
     ), f"new_token - roles {response.text} - {response.status_code}"
 
     # verify permission denied without required action
-    response = await test_client.get("/testing/groups", headers=headers)
+    response = test_client.get("/testing/groups", headers=headers)
     assert (
         response.status_code == 200
     ), f"new_token - groups {response.text} - {response.status_code}"
@@ -277,24 +274,24 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     assert "token_id" in decoded_token[1], f"{decoded_token}"
 
     # revoke token & test access failure
-    response = await test_client.delete(
+    response = test_client.delete(
         f"/auth/token?token_id={decoded_token[1]['token_id']}", headers=headers
     )
     assert (
         response.status_code == 200
     ), f"revoke token {response.text} - {response.status_code}"
 
-    response = await test_client.get("/testing/actions", headers=headers)
+    response = test_client.get("/testing/actions", headers=headers)
     assert (
         response.status_code == 403
     ), f"revoke_token - actions - {response.status_code} - {response.text}"
 
-    response = await test_client.get("/testing/roles", headers=headers)
+    response = test_client.get("/testing/roles", headers=headers)
     assert (
         response.status_code == 403
     ), f"revoke_token - roles {response.status_code} - {response.text}"
 
-    response = await test_client.get("/testing/groups", headers=headers)
+    response = test_client.get("/testing/groups", headers=headers)
     assert (
         response.status_code == 403
     ), f"revoke_token - groups - {response.status_code} - {response.text}"
@@ -303,7 +300,7 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     good_credentials = {"username": "admin", "password": "easyauth"}
 
-    response = await test_client.post("/auth/token/login", json=good_credentials)
+    response = test_client.post("/auth/token/login", json=good_credentials)
     assert response.status_code == 200, f"{response.text} - {response.status_code}"
 
     token = response.json()
@@ -311,7 +308,7 @@ async def test_server_authentication(auth_test_server: AsyncClient):
     # verify endpoint access while using token
 
     headers = {"Authorization": f"Bearer {token['access_token']}"}
-    response = await test_client.get("/auth/export", headers=headers)
+    response = test_client.get("/auth/export", headers=headers)
     assert response.status_code == 200
 
     config = response.json()
@@ -322,12 +319,13 @@ async def test_server_authentication(auth_test_server: AsyncClient):
 
     # use exported config to import
     for Model in [Users, Groups, Roles, Actions]:
-        await Model.delete_many(await Model.all())
+        models = await Model.all()
+        [await m.delete() for m in models]
 
-    response = await test_client.post("/auth/import", headers=headers, json=config)
+    response = test_client.post("/auth/import", headers=headers, json=config)
     assert response.status_code == 200
 
-    response = await test_client.get("/auth/export", headers=headers)
+    response = test_client.get("/auth/export", headers=headers)
     assert response.status_code == 200
 
     post_import_config = response.json()
