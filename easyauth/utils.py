85799e923856ec7f20a5491049a0587771e808c8
ForeztGump <foreztgump+git@slmail.me>
2023-04-28 14:04:36 -0700

    add error handling in getuser()

--- easyauth/utils.py
+++ easyauth/utils.py
@@ -4,14 +4,17 @@ from fastapi import Depends, Request
 
 def get_user():
     def get_user_handler(request: Request):
-        if "token" not in request.cookies and not "Authorization" in request.headers:
+        if "token" not in request.cookies and "Authorization" not in request.headers:
             return None
 
         if "token" in request.cookies:
-            decoded_token = jwt.decode(
-                request.cookies["token"], options={"verify_signature": False}
-            )
-            return decoded_token["permissions"]["users"][0]
+            try:
+                decoded_token = jwt.decode(
+                    request.cookies["token"], options={"verify_signature": False}
+                )
+                return decoded_token["permissions"]["users"][0]
+            except jwt.exceptions.DecodeError:
+                return None
         elif "Authorization" in request.headers:
             # header should be separated by 'Bearer <tokenstr>'
             decoded_token = jwt.decode(
